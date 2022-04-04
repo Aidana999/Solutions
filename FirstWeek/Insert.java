51c75b363f2290a5fb1fff5480365f8c0663447a
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-04-04 17:05:31 +0600

    Create Insert.java

---
+++ FirstWeek/Insert.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String E1 = " EpamJava ";
+        StringBuilder E2 = new StringBuilder(E1);
+        E2.insert(5, ' ');
+        System.out.println(E2);
+    }
+}
