dfa16216fc63a5b2590f3de989149980df6f6aa0
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-04-04 12:04:09 +0600

    Update ReverseStr.java

--- FirstWeek/ReverseStr.java
+++ FirstWeek/ReverseStr.java
@@ -1,1 +1,11 @@
+package com.company;
 
+public class Main {
+
+    public static void main(String[] args) {
+        String E1 = " Epam Java ";
+        StringBuilder E2 = new StringBuilder(E1);
+        E2.reverse();
+        System.out.println(E2);
+    }
+}
