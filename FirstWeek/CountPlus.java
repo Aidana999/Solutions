8411d4a030b70d2190ba9a927a3e06a2c36c20fa
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-04-12 18:38:27 +0600

    Update CountPlus.java

--- FirstWeek/CountPlus.java
+++ FirstWeek/CountPlus.java
@@ -1,1 +1,20 @@
+package com.company;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        String lilWord = scan.nextLine();
+        Pattern patt = Pattern.compile(lilWord);
+        Matcher mat = patt.matcher(text);
+        int counter = 0;
+        while (mat.find()) {
+            counter++;
+        }
+        System.out.println(counter);
+    }
+}
