12d0d2e61dc1f0ebd0e448700c620ab9295455ef
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-04-13 15:27:10 +0600

    Update ReverseWord.java

--- FirstWeek/ReverseWord.java
+++ FirstWeek/ReverseWord.java
@@ -1,1 +1,18 @@
+package com.company;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String E1 = scan.nextLine();
+        String[] word = E1.split(" ");
+        StringBuilder app = new StringBuilder();;
+        StringBuilder rev;
+        for (int i = 0; i < word.length; i++) {
+            rev= new StringBuilder(word[i]);
+            app.append(rev.reverse() + " ");
+        }
+        System.out.println(app.toString());
+    }
+}
