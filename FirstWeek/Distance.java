227b9490c4c1514d7182c4aa71898e38df57e8f9
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-04-13 15:10:06 +0600

    Update Distance.java

--- FirstWeek/Distance.java
+++ FirstWeek/Distance.java
@@ -1,1 +1,17 @@
+package com.company;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        String one = scan.next();
+        String two = scan.next();
+        int first = text.indexOf(one);
+        int second = text.indexOf(two);
+        int distance ;
+        distance = (second - first - one.length());
+            System.out.println(distance);
+    }
+}
