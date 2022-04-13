9a6431a172dc61f19c6ce97eb3d129dd2ddb1e4f
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-04-13 14:57:56 +0600

    Update OddEven.java

--- FirstWeek/OddEven.java
+++ FirstWeek/OddEven.java
@@ -1,1 +1,21 @@
+package com.company;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String E1 = scan.nextLine();
+        StringBuilder even = new StringBuilder();
+        StringBuilder odd = new StringBuilder();
+        for (int i = 0; i < E1.length(); i++) {
+            if (i % 2 == 0) {
+                even.append(E1.charAt(i));
+            } else {
+                odd.append(E1.charAt(i));
+            }
+        }
+        System.out.println("Even: " + odd);
+        System.out.println("Odd: " + even);
+    }
+}
