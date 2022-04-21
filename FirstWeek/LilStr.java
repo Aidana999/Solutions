02b61a9588f3e403953a0ab19fa746a3d3b8033a
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-04-21 19:08:52 +0600

    Update LilStr.java

--- FirstWeek/LilStr.java
+++ FirstWeek/LilStr.java
@@ -1,1 +1,19 @@
+package com.company;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String word = scan.nextLine();
+        Scanner scanner = new Scanner(word);
+        String[] splitStr = scanner.nextLine().split("\\s");
+        int len = splitStr[0].length();
+        String lilStr = splitStr[0];
+        for (int i = 0; i < splitStr.length; i++) {
+            if (splitStr[i].length() < len);
+            lilStr = splitStr[i];
+        }
+        System.out.println(lilStr);
+    }
+}
